bb7bad1d981c5e94b82b9f1bb77d9bc54fac0baf
Fadi <fady-naddaf@hotmail.com>
2023-03-05 11:26:02 +0100

    Creating Account page

---
+++ client/src/views/AccessAccount.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-link
+      class="primaryLink mt-24"
+      to="/"
+      ><h1 class="text-lg font-semibold text-gray-300">Login</h1></router-link
+    >
+    <router-link
+      class="secondaryLink mt-3"
+      to="/about"
+      ><h1 class="text-lg font-semibold text-gray-300">SignUp</h1></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name:"AccessAccount"
+}
+</script>
+
+<style>
+
+</style>
