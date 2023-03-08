3a91d6ccceb4aca8b0f701a984bc55d3747c665d
Fadi <fady-naddaf@hotmail.com>
2023-03-08 16:44:43 +0100

    fixing the google auth

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,58 @@
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import router from "./router";
+// import store from "./store";
+// import "./assets/tailwind.css";
+
+// import GAuth from 'vue-google-oauth2'
+// const gauthOption = {
+//   clientId: process.env.MIX_GOOGLE_CLIENT_ID,
+//   scope: 'profile email',
+//   prompt: 'consent'
+// }
+
+// createApp(App).use(store).use(router).mount("#app").use(GAuth, gauthOption);
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import gAuthPlugin from 'vue3-google-oauth2'
 
-createApp(App).use(store).use(router).mount("#app");
+const gauthOption = {
+  // clientId: process.env.MIX_GOOGLE_CLIENT_ID,
+  clientId: '830401754547-pk8426v0d7gp0ss0pd3vs2ou40vqnfmh.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'consent',
+  fetch_basic_profile: true
+}
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount("#app");
+app.use(gAuthPlugin, gauthOption);
+
+
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// import router from "./router";
+// import store from "./store";
+// import "./assets/tailwind.css";
+// import GAuth from 'vue-google-oauth2'
+
+
+// const gauthOption = {
+//   clientId: '830401754547-pk8426v0d7gp0ss0pd3vs2ou40vqnfmh.apps.googleusercontent.com',
+//   scope: 'profile email',
+//   prompt: 'consent',
+//   fetch_basic_profile: true
+// }
+// const gauthInstance = GAuth(gauthOption)
+
+// const app = createApp(App)
+// app.use(store)
+// app.use(router)
+// app.use(gauthInstance)
+// app.mount("#app")
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -5,42 +5,73 @@
       <FormInput v-for="input in inputs" :key="input.id" :placeholder="input.placeholder"/>
       <button class="secondaryLink rounded-lg text-white font-semibold mt-4">Login</button>
       <span class="text-sm mt-3 text-gray-300">Don't have an account?
-        <router-link class="pl-2 text-md text-secondary-100 font-semibold" to="/">Signup</router-link>
+        <router-link class="pl-2 text-md text-secondary-100 font-semibold" to="/">SignUp</router-link>
       </span>
       <div class="flex items-center justify-between w-full mt-5">
         <span class="w-1/2 h-0.5 bg-gray-300"></span>
         <p class="font-bold p-3 text-gray-300">OR</p>
         <span class="w-1/2 h-0.5 bg-gray-300"></span>
       </div>
-        <div class="g-signin2 m-3" data-onsuccess="onSignIn"></div>
+      <h1>initialized: {{ Vue3GoogleOauth.isInit }}</h1>
+      <button @click="handelSignIn">Sign in with google</button>
     </form>
   </div>
 </template>
 
 <script>
 import FormInput from "../components/FormInput.vue"
+import { gapi } from 'gapi-script';
+import router from '../router'
+import { inject } from 'vue'
 export default {
 name: "LoginPage",
-data() {
-  const inputs = [{
-    id: 1,
-    name: 'username',
-    type: 'text',
-    placeholder: 'Username'
+  data() {
+    const inputs = [{
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'Username'
+    },
+    {
+      id: 2,
+      name: 'password',
+      type: 'text',
+      placeholder: 'Password'
+    }]
+    return {
+      inputs,
+      isLoading: false,
+    }
   },
-  {
-    id: 2,
-    name: 'password',
-    type: 'text',
-    placeholder: 'Password'
-  }]
-  return {
-    inputs
+  setup() {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+    return {
+      Vue3GoogleOauth,
+      user: ''
+    }
+  },
+  methods: {
+    async handelSignIn() {
+      try {
+        gapi.load('auth2', async () => {
+          const googleAuth = gapi.auth2.getAuthInstance();
+          const googleUser = await googleAuth.signIn();
+          if(!googleUser) {
+            return null
+          } else {
+            this.user = googleUser.getBasicProfile().getEmail()
+            router.push('/');
+            console.log("google user", googleUser);
+          }
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  components: {
+    FormInput
   }
-},
-components: {
-  FormInput
-}
 }
 </script>
 
