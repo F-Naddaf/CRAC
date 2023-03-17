5401d4af2c1c8fb55fb094a5a3d47ccbadb797a2
Fadi <fady-naddaf@hotmail.com>
2023-03-17 18:55:29 +0100

    Delete config file from server

--- server/src/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const accountSid = process.env.TWILIO_ACCOUNT_SID;
-export const authToken = process.env.TWILIO_AUTH_TOKEN;
-// export const ServiceSID = VAf3d3b8c64a0452d52c1089ddce1e7049;
