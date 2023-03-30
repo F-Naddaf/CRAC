aae7e51de8f22ede2c71fabb7c896cac3d502e23
Fadi <fady-naddaf@hotmail.com>
2023-03-30 16:01:40 +0200

    Fix the Talk Bubble Shape, replace logo, label style, show password icon and build the video component

--- client/src/views/RecordVideo.vue
+++ client/src/views/RecordVideo.vue
@@ -1,35 +1,105 @@
 <template>
   <div>
-    <Camera :resolution="{ width: 375, height: 812 }" ref="camera" autoplay />
-    <button class="btn" @click="snapshot">Record a video</button>
+    <video
+      v-if="toggleCamera"
+      class="w-screen h-screen"
+      id="video"
+      autoplay
+    ></video>
+    <button
+      type="button"
+      id="startRecord"
+      v-if="cameraEnabled"
+      class="w-12 h-12 rounded-full bg-red-900"
+      @click="recordVideo"
+    ></button>
+    <button
+      type="button"
+      id="stopRecord"
+      v-else
+      class="w-12 h-12 rounded-full bg-yellow-300"
+      @click="stopRecording"
+    ></button>
+    <p class="recording" v-if="recording">Recording...</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
-import Camera from "simple-vue-camera";
-
 export default {
   name: "RecordVideo",
-  setup() {
-    const camera = ref(null);
-
-    const snapshot = async () => {
-      const blob = await camera.value?.snapshot();
-      const url = URL.createObjectURL(blob);
-      console.log("url", url);
-    };
+  data() {
     return {
-      camera,
-      snapshot,
+      videoStream: null,
+      mediaRecorder: null,
+      blob: null,
+      cameraEnabled: true,
+      recording: false,
     };
+  },
+  mounted() {
+    this.toggleCamera();
+  },
+  methods: {
+    async toggleCamera() {
+      try {
+        this.videoStream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        document.getElementById("video").srcObject = this.videoStream;
+        this.toggleCamera = true;
+      } catch (err) {
+        console.error("Error accessing user media:", err);
+        this.cameraEnabled = false;
+      }
+    },
+    recordVideo() {
+      this.mediaRecorder = new MediaRecorder(this.videoStream);
+      this.blob = [];
+      this.mediaRecorder.addEventListener("dataavailable", (e) => {
+        this.blob.push(e.data);
+      });
+      this.mediaRecorder.start();
+      this.cameraEnabled = false;
+      this.recording = true;
+    },
+    stopRecording() {
+      this.mediaRecorder.stop();
+      const blob = new Blob(this.blob, { type: "video/mp4" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "recording.mp4";
+      a.click();
+      this.cameraEnabled = true;
+      this.recording = false;
+    },
   },
 };
 </script>
 
 <style scoped>
-.btn {
-  color: white;
-  background-color: red;
+video {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+#startRecord,
+#stopRecord {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translate(-50%);
+}
+.recording {
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translate(-50%);
+  font-size: 12px;
+  color: red;
 }
 </style>
