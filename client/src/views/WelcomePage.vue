8675f25a252dab7179a1d74addc9db812d3e7c93
Fadi <fady-naddaf@hotmail.com>
2023-04-09 16:45:19 +0200

    Add animation to the welcome page

--- client/src/views/WelcomePage.vue
+++ client/src/views/WelcomePage.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="welcomeCard">
-    <p class="text-gray-500 text-lg mb-9">Welcome to</p>
-    <img class="w-full" src="../../public/img/logo.png" alt="logo" />
+    <p text-data="Welcome" class="greeting">Welcome</p>
+    <div class="relative w-full">
+      <img class="w-full" src="../../public/img/logo.png" alt="logo" />
+      <img class="animation-logo" src="../../public/img/logo.png" alt="logo" />
+    </div>
     <router-link class="primaryLink mt-24" to="/login"
       ><h1 class="text-lg font-semibold text-gray-300">
         Get Start
@@ -15,3 +18,70 @@ export default {
   name: "WelcomePage",
 };
 </script>
+
+<style scoped>
+.greeting {
+  position: relative;
+  font-size: 18px;
+  margin-bottom: 36px;
+  color: rgba(0, 0, 0, 0.1);
+  justify-self: center;
+}
+.greeting::before {
+  content: attr(text-data);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
+  color: #ba2f74;
+  border-right: 2px solid #ba2f74;
+  overflow: hidden;
+  animation: animate 3s linear;
+}
+@keyframes animate {
+  0%,
+  10% {
+    width: 0;
+  }
+  70% {
+    width: 100%;
+  }
+  99% {
+    border-right: 2px solid #ba2f74;
+  }
+  100% {
+    width: 100%;
+    border-right: none;
+  }
+}
+
+.greeting::before {
+  animation-fill-mode: forwards;
+}
+.animation-logo {
+  content: "";
+  animation-name: logo-animation;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+@keyframes logo-animation {
+  0% {
+    transform: scale(1);
+    filter: blur(0);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(1.3);
+    filter: blur(5px);
+    opacity: 0;
+  }
+}
+</style>
